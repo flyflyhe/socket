cb9ad8fe0ca08d501acd764a128350d6de3f91b7
jinxue <hejinxue001@163.com>
2016-11-28 21:42:48 +0800

    多进程异步操作

---
+++ pcntl/write.php
@@ -0,0 +1,15 @@
+<?php
+for ($i = 1; $i <= 5; ++$i) { 
+    $pid = pcntl_fork(); 
+
+    if ($pid == -1) {
+        
+    } else if ($pid) {
+        echo $pid.'进程开启'.PHP_EOL;
+    } else {
+        while(1) {
+            sleep(5);
+            file_put_content('/tmp/test'.$pid.log, date('Y-m-d H:i:s'), FILE_APPEND);
+        }
+    }
+}
