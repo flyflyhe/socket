939d85ccd57bfd3ff63a206ee2f17373a6ca4d89
jinxue <hejinxue001@163.com>
2016-11-28 21:46:10 +0800

    .log

--- pcntl/write.php
+++ pcntl/write.php
@@ -9,7 +9,7 @@ for ($i = 1; $i <= 5; ++$i) {
     } else {
         while(1) {
             sleep(5);
-            file_put_contents('/tmp/test'.$pid.log, date('Y-m-d H:i:s'), FILE_APPEND);
+            file_put_contents('/tmp/test'.$pid.'log', date('Y-m-d H:i:s'), FILE_APPEND);
         }
     }
 }
