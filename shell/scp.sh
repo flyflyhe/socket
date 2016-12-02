42861b4b958258b836cb310f59008cd19e2ab720
mrHe <hejixnue001@163.com>
2016-12-02 11:56:44 +0800

    scp备份文件

--- shell/scp.sh
+++ shell/scp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 source /etc/profile
 today=$(date +%Y-%m-%d)
-/usr/bin/scp -P 2208 -v root@112.74.182.208:/tmp/bak_trusted_list12.php /home/fly/bak/12shouxin.php.$today >> /home/fly/bak/scp.log
+/usr/bin/scp -P 端口 -v root@127.0.0.1:/tmp/bak_trusted_list12.php /home/fly/bak/12shouxin.php.$today >> /home/fly/bak/scp.log
 
 
-####
-SHELL=/bin/bash
-PATH=/sbin:/bin:/usr/sbin:/usr/bin
-MAILTO=fly
-HOME=/home/fly
-00 12 * * * sh /home/fly/flywww/socket/shell/scp.sh >> /home/fly/bak/scp.log
+#### crontab编辑 crontab 与 scp自动备份文件
+#crontab -e
+#SHELL=/bin/bash
+#PATH=/sbin:/bin:/usr/sbin:/usr/bin
+#MAILTO=fly
+#HOME=/home/fly
+#00 12 * * * sh /home/fly/flywww/socket/shell/scp.sh >> /home/fly/bak/scp.log
