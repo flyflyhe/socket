355efd75f95ff699712ef0ea9fb855b70511b17c
满仓 <hejinxue001@lonlife.cn>
2021-07-07 11:06:20 +0800

    spl arr

--- php/array/unset.php
+++ php/array/unset.php
@@ -6,3 +6,11 @@ echo "count:", count($arr), PHP_EOL;
 unset($arr[1]);
 print_r($arr);
 echo "count:", count($arr), PHP_EOL;
+
+$list = new SplFixedArray(3);
+$list[0] = 1;
+$list[1] = 2;
+$list[2] = 3;
+print_r($list);
+$list->offsetUnset(1);
+print_r($list);
