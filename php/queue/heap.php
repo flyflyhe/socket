7c3beba77e9fa125889c59b2a99c2585a282ce8f
满仓 <hejinxue001@lonlife.cn>
2021-07-08 10:24:33 +0800

    堆排序

--- php/queue/heap.php
+++ php/queue/heap.php
@@ -2,9 +2,18 @@
 
 
 $heap = new SplMinHeap();
-$heap->insert([22, 333]);
-$heap->insert([22, 33]);
-$heap->insert([222, 3]);
+$heap->insert(array_reverse(['a', 333]));
+$heap->insert(array_reverse(['b', 33]));
+$heap->insert(array_reverse(['c', 3]));
+$heap->insert(array_reverse(['d', 35]));
+$heap->insert(array_reverse(['e', 3911]));
+$heap->insert(array_reverse(['f', 234545]));
+$heap->insert(array_reverse(['g', 21]));
+$heap->insert(array_reverse(['h', 343]));
+$heap->insert(array_reverse(['i', 9]));
+$heap->insert(array_reverse(['j', 7]));
+$heap->insert(array_reverse(['k', 1]));
+$heap->insert(array_reverse(['l', 0]));
 
 //var_export($heap->extract());
 //var_export($heap->extract());
@@ -12,5 +21,5 @@ $heap->insert([222, 3]);
 
 $heap->rewind();
 foreach ($heap as $v) {
-    var_dump($v);
+    echo $v[0], $v[1], PHP_EOL;
 }
